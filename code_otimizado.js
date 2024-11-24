df49564a12ebd1c757c1abdb227b927bdbb63567
Geilson Lucas <geilson.gears@outlook.com>
2024-11-24 12:43:41 -0300

    Create code_otimizado.js

---
+++ code_otimizado.js
@@ -0,0 +1,17 @@
+function geraAleatorios(quantidade){
+
+var vetorNumeros = [];
+var vetorNumSel = [];
+
+for(var j = 1; j <= 60; j++){
+    vetorNumeros.push(j)
+}
+
+for(var i = 0; i < quantidade; i++){
+    var aleatorio = Math.floor(Math.random()*vetorNumeros.length)
+    var numSel = vetorNumeros.splice(aleatorio,1)
+    vetorNumSel = [...vetorNumSel, ...numSel]
+}
+
+console.log(vetorNumSel)
+}
